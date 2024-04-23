302e86dd383484e7413519c06af27f2f6ff03559
JonathanYe3 <jonathanye2003@gmail.com>
2024-04-23 17:15:21 -0400

    the overfitting ever

--- AdaBoostWeak.py
+++ AdaBoostWeak.py
@@ -1,156 +1,129 @@
+# thank you https://github.com/AlvaroCorrales/AdaBoost/blob/main/AdaBoost.py
+# Imports
 import numpy as np
-import math
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
 
-class DecisionStump:
-    def __init__(self):
-        #initialize the stumps
-        self.polarity = 1
-        self.axis = None
-        self.threshold = None
-
-        self.alpha = None
-
-
-
-class AdaBoostWeak:
-    def __init__(self, n_learners, type2penalty = False, lr = 0.01):
-        self.n_learners = n_learners
-        self.type2penalty = type2penalty
-        self.lr = lr
-
-        #classifiers
-        self.clfs = []
-
-    # def find_next_weak_learner(self):
-    #     clf = DecisionStump()
-    #     min_error = math.inf
-    #     feature_size = self.X.shape[1]
-
-    #     for i in range(feature_size):
-    #         #feature_vals = np.expand_dims(self.X[:, i], axis=1)
-    #         feature_vals = np.expand_dims(self.X[:, i].A, axis=1)
-
-    #         unique_vals = np.unique(feature_vals)
-
-    #         for thresh in unique_vals:
-    #             prediction = np.ones(shape=self.y.shape[0])
-    #             #print((self.X[:, i].A < thresh).shape)
-    #             ham_idx = (self.X[:, i].A < thresh).flatten()
-    #             prediction[ham_idx] = -1
-
-    #             error = sum(self.weights[prediction != self.y])
-
-    #             if error > 0.5:
-    #                 error = 0.5 - error
-    #                 polarity = -1
-    #             else:
-    #                 polarity = 1
-
-    #             if error < min_error:
-    #                 clf.polarity = polarity
-    #                 clf.threshold = thresh
-    #                 clf.axis = i
-    #                 min_error = error
-
-    #     clf.alpha = (0.5) * math.log((1.0 - min_error + 1e-8) / (min_error + 1e-8)) # small epsilon moment
-
-    #     #Compute prediction using current clf - final vote moment
-    #     pred = np.ones(np.shape(self.y))
-    #     neg_idx = (clf.polarity * self.X[:, clf.axis] < clf.polarity * clf.threshold) # CHANGE THIS LATER FOR THE PENALTY
-    #     pred[neg_idx] = -1 #WE MAY NEED TO REFACTOR OUR DATA - -1 AND 1
-
-    #     #update sample weights
-    #     self.weights *= np.exp(-clf.alpha * self.y * pred) # perhaps learning rate here
-    #     self.weights /= np.sum(self.weights)
-
-    #     # Append the weak classifier to the classifier list
-    #     self.clfs.append(clf)
-
-    def find_next_weak_learner(self):
-        clf = DecisionStump()
-        min_error = math.inf
-        feature_size = self.X.shape[1]
-
-        for i in range(feature_size):
-            feature_vals = self.X[:, i].A
-            unique_vals = np.unique(feature_vals)
-
-            # Calculate predictions for all thresholds at once - this doesn't do what I want it to
-            predictions = np.ones((2, self.y.shape[0]))
-            print("predictions shape ", predictions.shape)
-            ham_idx = (feature_vals < unique_vals[:, None]).T
-            print("ham_idx shape ", ham_idx.shape)
-            predictions[ham_idx] = -1
-
-            # Calculate errors for all thresholds at once
-            errors = np.sum(self.weights * (predictions != self.y), axis=1)
-
-            # Find the threshold with the minimum error
-            min_error_idx = np.argmin(errors)
-            min_error = errors[min_error_idx]
-            best_thresh = unique_vals[min_error_idx]
-
-            # Determine the polarity
-            if min_error > 0.5:
-                min_error = 0.5 - min_error
-                polarity = -1
-            else:
-                polarity = 1
-
-            # Update the classifier
-            clf.polarity = polarity
-            clf.threshold = best_thresh
-            clf.axis = i
-
-            clf.alpha = (0.5) * np.log((1.0 - min_error + 1e-8) / (min_error + 1e-8)) # small epsilon moment
-
-            # Compute prediction using current clf - final vote moment
-            pred = np.ones(np.shape(self.y))
-            neg_idx = (clf.polarity * feature_vals < clf.polarity * clf.threshold).flatten() # CHANGE THIS LATER FOR THE PENALTY
-            pred[neg_idx] = -1 #WE MAY NEED TO REFACTOR OUR DATA - -1 AND 1
-
-            # Update sample weights
-            self.weights *= np.exp(-clf.alpha * self.y * pred) # perhaps learning rate here
-            self.weights /= np.sum(self.weights)
-
-            # Append the weak classifier to the classifier list
-            self.clfs.append(clf)
-
-
-
-    def fit(self, X, y):
-        self.X = X
-        self.y = y # need to change to -1, 1
-        self.y[self.y == 0] = -1
-        self.N = X.shape[0]
-
-        #intiialize weights
-        self.weights = np.ones(self.N) / self.N
-
-        print(f'starting weights: \n {self.weights}')
-
-        for i in range(self.n_learners):
-            self.find_next_weak_learner()
-            if (i+1)%10 == 0:
-                print(f'learner {i+1}')
-
-    def predict(self, X):
-        clf_preds = [clf.alpha * self.stump_predict(clf, X) for clf in self.clfs]
-        y_pred = np.sum(clf_preds, axis=0)
-        y_pred = np.sign(y_pred)
-
-        # change to 0 1 labels
-        y_pred[y_pred == -1] = 0
-
-        return y_pred
-
-    def stump_predict(self, clf, X):
-        m = X.shape[0]
-        pred = np.ones(m)
-        neg_idx = (clf.polarity * X[:, clf.axis] < clf.polarity * clf.threshold)
-        pred[neg_idx] = -1
-
-        return pred
+# Helper functions
+def compute_error(y, y_pred, w_i):
+    '''
+    Calculate the error rate of a weak classifier m. Arguments:
+    y: actual target value
+    y_pred: predicted value by weak classifier
+    w_i: individual weights for each observation
 
     
+    Note that all arrays should be the same length. Convert sparse array to regular array
+    '''
+
+    return (sum(w_i * (np.not_equal(y, y_pred)).astype(int)))/sum(w_i)
+
+def compute_alpha(error):
+    '''
+    Calculate the weight of a weak classifier m in the majority vote of the final classifier. This is called
+    alpha in chapter 10.1 of The Elements of Statistical Learning. Arguments:
+    error: error rate from weak classifier m
+    '''
+    return np.log((1 - error) / error)
+
+def update_weights(w_i, alpha, y, y_pred):
+    ''' 
+    Update individual weights w_i after a boosting iteration. Arguments:
+    w_i: individual weights for each observation
+    y: actual target value
+    y_pred: predicted value by weak classifier  
+    alpha: weight of weak classifier used to estimate y_pred
+    '''  
+    return w_i * np.exp(alpha * (np.not_equal(y, y_pred)).astype(int))
+
+# Define AdaBoost class
+class AdaBoostWeak:
+    
+    def __init__(self, rounds, type2penalty):
+        # self.w_i = None
+        self.alphas = []
+        self.stumps = []
+        self.rounds = rounds
+        self.training_errors = []
+        self.prediction_errors = []
+        self.type2penalty = type2penalty
+
+    def fit(self, X, y):
+        '''
+        Fit model. Arguments:
+        X: independent variables
+        y: target variable
+        rounds: number of boosting rounds. Default is 100
+        '''
+        
+        # Clear before calling
+        self.alphas = [] 
+        self.training_errors = []
+        y[y == 0] = -1
+
+        # Iterate over M weak classifiers
+        for m in range(0, self.rounds):
+            
+            # Set weights for current boosting iteration
+            if m == 0:
+                w_i = np.ones(len(y)) * 1 / len(y)  # At m = 0, weights are all the same and equal to 1 / N
+            else:
+                w_i = update_weights(w_i, alpha_m, y, y_pred)
+            # print(w_i)
+            
+            # (a) Fit weak classifier and predict labels
+            stump = DecisionTreeClassifier(max_depth = 1)     # Stump: Two terminal-node classification tree
+            stump.fit(X, y, sample_weight = w_i)
+            y_pred = stump.predict(X)
+            
+            self.stumps.append(stump) # Save to list of weak classifiers
+
+            # (b) Compute error
+            error_m = compute_error(y, y_pred, w_i)
+            self.training_errors.append(error_m)
+            # print(error_m)
+
+            # (c) Compute alpha
+            alpha_m = compute_alpha(error_m)
+            self.alphas.append(alpha_m)
+            # print(alpha_m)
+
+        assert len(self.stumps) == len(self.alphas)
+
+
+    def predict(self, X):
+        '''
+        Predict using fitted model. Arguments:
+        X: independent variables
+        '''
+
+        # Initialize an array to store the final predictions
+        final_predictions = np.zeros(X.shape[0])
+
+        # Predict class label for each weak classifier, weighted by alpha_m
+        for m in range(self.rounds):
+            y_pred_m = self.stumps[m].predict(X) * self.alphas[m]
+            final_predictions += y_pred_m
+
+        # Estimate final predictions
+        y_pred = np.sign(final_predictions).astype(int) # need to change -1 to 0  
+        y_pred[y_pred == -1] = 0
+        return y_pred
+
+      
+    def error_rates(self, X, y):
+        '''
+        Get the error rates of each weak classifier. Arguments:
+        X: independent variables
+        y: target variables associated to X
+        '''
+        
+        self.prediction_errors = [] # Clear before calling
+        
+        # Predict class label for each weak classifier
+        for m in range(self.rounds):
+            y_pred_m = self.stumps[m].predict(X)          
+            error_m = compute_error(y = y, y_pred = y_pred_m, w_i = np.ones(len(y)))
+            self.prediction_errors.append(error_m)
+
 
